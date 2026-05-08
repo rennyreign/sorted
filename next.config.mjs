011f096d205d9f619a74f041c5523e3cd3d30228
rennyreign <renaldo@adxengine.net>
2026-05-08 08:09:57 +0100

    chore: configure for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost"],
-  output: "export",
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "/sorted" : "",
   images: {
     unoptimized: true,
   },
